38d48d2ee03cd1f8f909d43df5405435c6d93860
Nikita Khandekar <nikki.khandekar@gmail.com>
2021-02-03 00:16:54 +0530

    mock data

--- src/app/helpster.service.ts
+++ src/app/helpster.service.ts
@@ -6,10 +6,19 @@ import { User } from './User';
   providedIn: 'root'
 })
 export class HelpsterService {
-  helpsters: Array<User>
+  users: Array<User>
+  services: Array<string>
+  categories: Map<string, Array<string>>
+
 
   constructor() {
-    this.helpsters = new Array()
+    this.users = new Array();
+    this.services = ["Household", "Outdoor", "Farm", "Transport"];
+    this.categories = new Map();
+    this.categories.set("Household", ["Cooking", "Cleaning", "Washing"])
+    this.categories.set("Outdoor", ["Security Gauard", "Gardening"])
+    this.categories.set("Farm", ["Watering", "Monitoring", "Crop Cultivation"])
+    this.categories.set("Transport", ["Car Cleaning", "Driving"])
   }
 
   id: number = 1
@@ -19,15 +28,15 @@ export class HelpsterService {
 
   registerHelpster(helpster: User): Observable<Boolean> {
     return new Observable((observer) => {
-      this.helpsters[this.helpsters.length] = helpster
+      this.users[this.users.length] = helpster
       observer.next(true)
     })
   }
 
   login(email: string, password: string): Observable<Boolean> {
     return new Observable((observer) => {
-      console.log("rajat length" + this.helpsters.length)
-      for (var helpster of this.helpsters) {
+      console.log("rajat length" + this.users.length)
+      for (var helpster of this.users) {
         console.log("rajat " + helpster.email)
         if (helpster.email == email) {
           if (helpster.password == password) {
@@ -41,14 +50,26 @@ export class HelpsterService {
     })
   }
 
+  getAllServices(): Observable<String[]> {
+    return new Observable((observer) => {
+      observer.next(this.services)
+    })
+  }
+
+  getAllCategories(service: string): Observable<String[]> {
+    return new Observable((observer) => {
+      observer.next(this.categories[service])
+    })
+  }
+
   getAllHelpster(): Observable<User[]> {
     return new Observable((observer) => {
-      observer.next(this.helpsters)
+      observer.next(this.users)
     })
   }
 
   addHelpster(h: User): Observable<User> {
-    this.helpsters[this.helpsters.length] = h
+    this.users[this.users.length] = h
     return new Observable((observer) => {
       observer.next(h)
     })
